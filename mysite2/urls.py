b069e4b0f8a4c1773a3266d14c3589ab40c146c2
onchin <ochin@unomaha.edu>
2018-09-27 22:40:22 -0500

    additional files and changes for heroku

--- mysite2/urls.py
+++ mysite2/urls.py
@@ -20,7 +20,6 @@ from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
-    # path('blog/', include('blog.urls', namespace='blog')),
+    path('blog/', include('blog.urls', namespace='blog')),
     path('', views.home)
 ]
